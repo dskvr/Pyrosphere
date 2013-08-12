5be18e05606bba5a3de3c85bdd1a1d636e202617
SEan <hi.na3s@gmail.com>
2013-08-11 18:44:04 -0700

    Line breaks.

--- Pyronode/app.js
+++ Pyronode/app.js
@@ -7,7 +7,7 @@ io.sockets.on('connection',function(socket){
     io.sockets.sockets['nickname'] = socket.id;
 		io.sockets.sockets['key'] = Math.round(Math.random*Math.random/Math.random)*9999999;
 		
-		if(total = 1) socket.set('sessionController', socket.id, function(){
+		if(total == 1) socket.set('sessionController', socket.id, function(){
 			
 		})
 		
@@ -18,11 +18,11 @@ io.sockets.on('connection',function(socket){
     client.on("pipe", function(data) {      
 	
         var sock_id = io.sockets.sockets['nickname'];
-				var active_id = socket.set('sessionController'); //This is the ID that has control over the Sphere.
+				var active_id = socket.get('sessionController'); //This is the ID that has control over the Sphere.
 				if(active_id != sock_id) io.sockets.sockets[sock_id].emit("session", { key  : false } );
 				// else serial.send(data.req);
-				// else io.sockets.sockets[sock_id].emit("private", "message");
-        
+				// else io.sockets.sockets[sock_id].emit("private", "message");     
 
     });
+
 });
